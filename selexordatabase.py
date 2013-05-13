ed9e186bbf8e7b17ecce838e37311159c5084b25
linkleonard <leon.wlaw@gmail.com>
2013-05-13 22:56:02 +0000

    Fully migrated to new database model

--- selexordatabase.py
+++ selexordatabase.py
@@ -13,21 +13,35 @@
   Probes various services to acquire the latest information about all vessels.
 
   Nodes Table:
-  [nodelocation][nodekey]
-  nodelocation is IP:Port
-  nodekey is the public key that uniquely identifies each node.
+  [node_id][node_key][node_port][ip_addr][last_ip_change][last_seen]
 
   vessels Table:
-  [nodelocation][vesselname]
+  [node_id][vesselname]
 
   userkey Table
-  [nodelocation][vesselname][Userkey]
-
-  location Table:
-  [nodelocation][country_code][city][longitude][latitude]
+  [node_id][vesselname][userkey]
 
   vesselports Table:
-  [nodelocation][vesselname][port]
+  [node_id][vesselname][port]
+
+  location Table:
+  [ip_addr][country_code][city][longitude][latitude]
+
+
+  Definitions:
+    node_id: The internal identifier for each node, as nodekeys are too long to use as indexes.
+    node_key: The public key that uniquely identifies each node.
+    node_port: The port the nodemanager of the node is listening on.
+    ip_addr: The IP address that the nodemanager of the node is listening on.
+    last_ip_change: The time of the last IP change of this node.
+    last_seen: The time this node was last seen.
+    userkey: The public key associated with a user.
+    port: A user port.
+
+    country_code: A 2-digit country code.
+    city: The name of the city.
+    longitude/latitude: Coordinates where an IP is associated with.
+
 
 <Dependencies>
   MySQLdb Python Module
@@ -55,10 +69,6 @@ import MySQLdb
 
 
 
-# The default value for a vessel's ip_change_count.
-# It is incremented everytime the node's IP address changes.
-# Therefore, when a node's IP is first registered, the ip_change_count is incremented to 0.
-INIT_IP_CHANGE_COUNT = -1
 
 
 def autoretry_mysql_command(cursor, command):
@@ -140,46 +150,12 @@ def create_database():
   Creates any missing tables.
   """
   db, cursor = selexorhelper.connect_to_db()
+  code = open('database_create.sql', 'r').read()
+  for line in code.split(';'):
+    if line.strip():
+      cursor.execute(line.strip())
 
-  # Do the tables we need exist?
-  cursor.execute("show tables")
-  result = cursor.fetchall()
 
-  existing_tables = []
-  for item in result:
-    existing_tables.append(item[0])
-
-  tables_contents = {
-    'nodes': """
-nodelocation varchar(40) NOT NULL PRIMARY KEY,
-nodekey text NOT NULL
-""",
-    'vessels': """
-nodelocation varchar(40) NOT NULL,
-vesselname varchar(10) NOT NULL
-""",
-    'userkeys': """
-nodelocation varchar(40) NOT NULL,
-vesselname varchar(10) NOT NULL,
-userkey text NOT NULL
-""",
-    'vesselports': """
-nodelocation varchar(40) NOT NULL,
-vesselname varchar(10) NOT NULL,
-port int NOT NULL
-""",
-    'location': """
-nodelocation varchar(40) NOT NULL,
-city varchar(50),
-country_code varchar(50),
-longitude double,
-latitude double
-"""
-  }
-
-  for table in tables_contents:
-    if table not in existing_tables:
-      cursor.execute("CREATE TABLE "+table+" ("+tables_contents[table]+")")
 
 
 def contact_vessels_and_update_database(nodes_to_check):
@@ -226,7 +202,7 @@ def contact_vessels_and_update_database(nodes_to_check):
 
         # Only retrieve the geographic information if we don't have it already
         # The geoip server's data doesn't change, so we don't need to constantly update it.
-        geoinfo_exists = autoretry_mysql_command(cursor, "SELECT nodekey FROM nodes WHERE nodelocation='"+nodelocation+"'") == 1L
+        geoinfo_exists = autoretry_mysql_command(cursor, "SELECT ip_addr FROM location WHERE ip_addr='"+nodeinfo['id']+"'") == 1L
         if not geoinfo_exists:
           logger.info("Location data not in database, looking up on geoip: "+nodelocation)
           geoinfo = geoip_record_by_addr(nodeinfo['id'])
@@ -239,7 +215,7 @@ def contact_vessels_and_update_database(nodes_to_check):
         geoinfo = {}
       format_geoinfo(geoinfo)
 
-      commit_data_to_database(db, cursor, nodelocation, node_dict, ports, geoinfo)
+      commit_data_to_database(db, cursor, nodeinfo['id'], nodeinfo['port'], node_dict, ports, geoinfo)
 
     except NMClientException, e:
       if not node_nmhandle:
@@ -259,78 +235,238 @@ def contact_vessels_and_update_database(nodes_to_check):
 
 
 
-def commit_data_to_database(db, cursor, nodelocation, node_dict, ports, geoinfo):
+def commit_data_to_database(db, cursor, node_ip, node_port, node_dict, ports, geoinfo):
   # Just in case we attempted to make any changes in a previous run and failed
   db.rollback()
   # == Update Nodes Table ==
   nodekeystr = rsa_publickey_to_string(node_dict['nodekey'])
 
-  new_node = autoretry_mysql_command(cursor, "SELECT nodekey FROM nodes WHERE nodelocation='"+nodelocation+"'") == 0L
+  new_node = autoretry_mysql_command(cursor, "SELECT node_id FROM nodes WHERE node_key='"+nodekeystr+"'") == 0L
 
   if new_node:
     # Node isn't recognized, add it to the db
-    autoretry_mysql_command(cursor, "INSERT INTO nodes (nodelocation, nodekey) VALUE ('" +
-        nodelocation + "', '" + nodekeystr + "')")
+    cmd = ("INSERT INTO nodes (node_key, ip_addr, node_port, last_ip_change, last_seen) "+
+          "VALUES ('%s', '%s', %i, NOW(), NOW())") % (nodekeystr, node_ip, node_port)
+    autoretry_mysql_command(cursor, cmd)
+
+    # Now retrieve the internal node_id.
+    autoretry_mysql_command(cursor, "SELECT node_id FROM nodes WHERE node_key='"+nodekeystr+"'")
+    node_id = cursor.fetchone()[0]
+    logger.info('\n'.join([
+        "New node found: #" + str(node_id),
+        "Nodekey:",
+        nodekeystr
+      ]))
   else:
-    # Handle already exists, does the nodekey still match?
-    if cursor.fetchone()[0] != nodekeystr:
-      autoretry_mysql_command(cursor, "UPDATE nodes SET nodekey='"+
-          nodekeystr+"' WHERE nodelocation='"+nodelocation+"'")
+    # Handle already exists
+    node_id = cursor.fetchone()[0]
+    logger.info('\n'.join([
+        "Updating node: #" + str(node_id),
+        "Nodekey:",
+        nodekeystr
+      ]))
+
+
+    # Did the IP address change?  If so mark that down.
+    autoretry_mysql_command(cursor, "SELECT (ip_addr) FROM nodes WHERE node_key='"+nodekeystr+"'")
+    old_node_ip = cursor.fetchone()[0]
+    if node_ip != old_node_ip:
+      cmd = "UPDATE nodes SET ip_addr='%s', last_ip_change=NOW() WHERE node_id=%i" % (node_ip, node_id)
+      autoretry_mysql_command(cursor, cmd)
+
+    # Update the last time we saw the node.
+    cmd = "UPDATE nodes SET last_seen=NOW() WHERE node_id=%i" % (node_id)
+    autoretry_mysql_command(cursor, cmd)
 
   # == Update Vessels Table ==
-  autoretry_mysql_command(cursor, "DELETE FROM vessels WHERE nodelocation='"+nodelocation+"'")
-  for vesselname in node_dict['vessels']:
-    # v2 is a special vessel, we can't use it
-    if vesselname == 'v2':
-      continue
-    autoretry_mysql_command(cursor, "INSERT INTO vessels (nodelocation, vesselname) VALUES ('"+nodelocation+"','"+vesselname+"')")
-
+  update_vessels_table(cursor, node_id, node_dict['vessels'])
+  
   # == Update Userkeys ==
-  for vesselname in node_dict['vessels']:
-    # v2 is a special vessel, we can't use it
-    if vesselname == 'v2':
-      continue
-
-    autoretry_mysql_command(cursor, "DELETE FROM userkeys WHERE nodelocation='"+nodelocation+"'")
-    for userkey in node_dict['vessels'][vesselname]['userkeys']:
-      userkeystr = rsa_publickey_to_string(userkey)
-      autoretry_mysql_command(cursor,
-          "INSERT INTO userkeys (nodelocation, vesselname, userkey) VALUE ('"+
-          nodelocation+"', '"+vesselname+"', '"+userkeystr+"')")
+  update_userkeys_table(cursor, node_id, node_dict['vessels'])
 
   # == Update Ports ==
-  for vesselname in node_dict['vessels']:
-    # v2 is a special vessel, we can't use it
-    if vesselname == 'v2':
-      continue
-
-    autoretry_mysql_command(cursor, "DELETE FROM vesselports WHERE nodelocation='"+nodelocation+"'")
-    for port in ports[vesselname]:
-      cursor.execute("INSERT INTO vesselports (nodelocation, vesselname, port) VALUE ('"+nodelocation+"', '"+vesselname+"',"+str(port)+")")
+  update_ports_table(cursor, node_id, ports)
 
   # == Update Location Table ==
-  # Only update location information if it is provided.
-  # Geoinfo is an empty dict if we don't have new information.
-  if geoinfo:
-    if new_node:
-      cursor.execute("INSERT INTO location (nodelocation) VALUE ('"+nodelocation+"')")
-
-    # Now update the database with the new information
-    # First, the string types...
-    for key in ['city', 'country_code']:
-      if key in geoinfo:
-        autoretry_mysql_command(cursor, "UPDATE location SET "+key+"='"+geoinfo[key]+"' WHERE nodelocation='"+nodelocation+"'")
-      else:
-        autoretry_mysql_command(cursor, "UPDATE location SET "+key+"=NULL WHERE nodelocation='"+nodelocation+"'")
-
-    # Now, the double types
-    for key in ['longitude', 'latitude']:
-      if key in geoinfo:
-        autoretry_mysql_command(cursor, "UPDATE location SET "+key+"="+str(geoinfo[key])+" WHERE nodelocation='"+nodelocation+"'")
-      else:
-        autoretry_mysql_command(cursor, "UPDATE location SET "+key+"=NULL WHERE nodelocation='"+nodelocation+"'")
+  if not geoinfo is None and geoinfo:
+    update_location_table(cursor, node_ip, geoinfo)
 
   db.commit()
+
+
+
+
+
+
+def update_vessels_table(cursor, node_id, vessel_dict):
+  query = 'SELECT vessel_name FROM vessels WHERE node_id='+str(node_id)
+  num_vessels = autoretry_mysql_command(cursor, query)
+  vessel_rows = cursor.fetchall()
+
+  if vessel_rows:
+    # Remove vessels that were lost, if any
+    vessels_to_remove = []
+    for [vessel_name] in vessel_rows:
+      if not vessel_name in vessel_dict:
+        # We pass this to MySQL later;  MySQL expects strings to be wrapped in quotes.
+        vessels_to_remove += ['"'+vessel_name+'"']
+
+    if vessels_to_remove:
+      logger.info('\n'.join([
+            "Node #"+str(node_id),
+            "Lost vessels: "+str(vessels_to_remove),
+            ]))
+
+      query = 'DELETE FROM vessels WHERE node_id='+str(node_id)+' AND vessel_name in ('+', '.join(vessels_to_remove)+')'
+      autoretry_mysql_command(cursor, query)
+
+  # Update the list of vessels
+  logger.info('\n'.join([
+      "Node #"+str(node_id),
+      "Current vessels: "+str(vessel_dict.keys()),
+      "Number of Vessels in database (excluding v2): " + str(num_vessels)
+      ]))
+
+  if vessel_dict:
+    # IGNORE keyword is to tell MySQL to ignore vessels that already exist.
+    query = 'INSERT IGNORE INTO vessels (node_id, vessel_name) VALUES'
+    for vessel_name in vessel_dict:
+      # v2 can never be used... No sense in tracking it in the vessel database.
+      if vessel_name == 'v2':
+        continue
+      query += " ('"+str(node_id)+"', '"+vessel_name+"'),"
+    # Get rid of the trailing comma after the last tuple
+    query = query.strip(',')
+    autoretry_mysql_command(cursor, query)
+
+
+def update_userkeys_table(cursor, node_id, vessel_dict):
+  query = 'SELECT vessel_name FROM userkeys WHERE node_id='+str(node_id)
+  num_vessels = autoretry_mysql_command(cursor, query)
+  vessel_rows = cursor.fetchall()
+
+  if vessel_rows:
+    # Remove vessels that were lost, if any
+    vessels_to_remove = []
+    for [vessel_name] in vessel_rows:
+      if not vessel_name in vessel_dict:
+        # We pass this to MySQL later;  MySQL expects strings to be wrapped in quotes.
+        vessels_to_remove += ['"'+vessel_name+'"']
+
+    if vessels_to_remove:
+      query = 'DELETE FROM userkeys WHERE node_id='+str(node_id)+' AND vessel_name in ('+', '.join(vessels_to_remove)+')'
+      autoretry_mysql_command(cursor, query)
+
+
+  # Update the userkeys
+  for vessel_name in vessel_dict:
+    # v2 can never be used... No sense in tracking it in the userkey database.
+    if vessel_name == 'v2':
+      continue
+
+
+    query = 'SELECT userkey FROM userkeys WHERE node_id='+str(node_id)+" AND vessel_name='"+vessel_name+"'"
+    autoretry_mysql_command(cursor, query)
+    userkey_rows = cursor.fetchall()
+
+    userkeys_to_remove = []
+    # Remove the userkeys that were lost, if any
+    for [userkey] in userkey_rows:
+      if not rsa_string_to_publickey(userkey) in vessel_dict[vessel_name]['userkeys']:
+        # We pass this to MySQL later;  MySQL expects strings to be wrapped in quotes.
+        userkeys_to_remove += ['"'+userkey+'"']
+
+    if userkeys_to_remove:
+      query = 'DELETE FROM userkeys WHERE (node_id, vessel_name)=('+str(node_id)+', "'+vessel_name+'") AND userkey in ('+', '.join(userkeys_to_remove)+')'
+      autoretry_mysql_command(cursor, query)
+
+
+    # Vessels may not have userkeys on them.  Don't bother adding them in that case.
+    if vessel_dict[vessel_name]['userkeys']:
+      # IGNORE keyword is to tell MySQL to ignore userkeys that already exist.
+      query = 'INSERT IGNORE INTO userkeys (node_id, vessel_name, userkey) VALUES'
+      for userkey in vessel_dict[vessel_name]['userkeys']:
+        query += " ('"+str(node_id)+"', '"+vessel_name+"', '"+rsa_publickey_to_string(userkey)+"'),"
+      # Get rid of the trailing comma after the last tuple
+      query = query.strip(',')
+      autoretry_mysql_command(cursor, query)
+
+
+
+def update_ports_table(cursor, node_id, ports):
+  query = 'SELECT vessel_name FROM vesselports WHERE node_id='+str(node_id)
+  num_vessels = autoretry_mysql_command(cursor, query)
+  vessel_rows = cursor.fetchall()
+
+  if vessel_rows:
+    # Remove vessels that were lost, if any
+    vessels_to_remove = []
+    for [vessel_name] in vessel_rows:
+      if not vessel_name in ports:
+        # We pass this to MySQL later;  MySQL expects strings to be wrapped in quotes.
+        vessels_to_remove += ['"'+vessel_name+'"']
+
+    if vessels_to_remove:
+      query = 'DELETE FROM vesselports WHERE node_id='+str(node_id)+' AND vessel_name in ('+', '.join(vessels_to_remove)+')'
+      autoretry_mysql_command(cursor, query)
+
+
+  # Update the ports
+  for vessel_name in ports:
+    # v2 can never be used... No sense in tracking it in the userkey database.
+    if vessel_name == 'v2':
+      continue
+
+    query = 'SELECT port FROM vesselports WHERE node_id='+str(node_id)+" AND vessel_name='"+vessel_name+"'"
+    autoretry_mysql_command(cursor, query)
+    ports_rows = cursor.fetchall()
+
+    ports_to_remove = []
+    # Remove the userkeys that were lost, if any
+    for [port] in ports_to_remove:
+      if not port in ports[vessel_name]:
+        # We pass this to MySQL later;  MySQL expects strings to be wrapped in quotes.
+        ports_to_remove += ['"'+port+'"']
+
+    if ports_to_remove:
+      query = 'DELETE FROM vesselports WHERE (node_id, vessel_name)=('+str(node_id)+', "'+vessel_name+'" AND port in ('+', '.join(ports_to_remove)+')'
+      autoretry_mysql_command(cursor, query)
+
+
+    # Vessels may not have ports on them.  Don't bother adding them in that case.
+    if ports[vessel_name]:
+      # IGNORE keyword is to tell MySQL to ignore userkeys that already exist.
+      query = 'INSERT IGNORE INTO vesselports (node_id, vessel_name, port) VALUES'
+      for port in ports[vessel_name]:
+        query += " ('"+str(node_id)+"', '"+vessel_name+"', "+str(port)+"),"
+      # Get rid of the trailing comma after the last tuple
+      query = query.strip(',')
+      autoretry_mysql_command(cursor, query)
+
+
+
+
+def update_location_table(cursor, ip_addr, geoinfo):
+  # City is not always defined
+  if 'city' in geoinfo:
+    city = geoinfo['city']
+  else:
+    city = ""
+
+  # == Update Userkeys ==
+  country_code = geoinfo['country_code']
+  longitude = str(geoinfo['longitude'])
+  latitude = str(geoinfo['latitude'])
+
+  query = 'INSERT INTO location (ip_addr, city, country_code, longitude, latitude) VALUES '
+  # Specifies the location tuple
+  query += "('%s', '%s', '%s', %s, %s) " % (ip_addr, city, country_code, longitude, latitude)
+  query += "ON DUPLICATE KEY UPDATE "
+  # Specifies the location tuple for the update clause
+  query += "city='%s', country_code='%s', longitude=%s, latitude=%s" % (city, country_code, longitude, latitude)
+
+  autoretry_mysql_command(cursor, query)
+
 
 
 
@@ -343,8 +479,11 @@ if __name__=='__main__':
 
   geoip_init_client()
 
-  # Create the databases if they haven't been created
-  create_database()
+  # Perform any first-time initialization if specified by the administrator.
+  if len(sys.argv) > 1 and sys.argv[1] == 'initialize':
+    # Create the databases if they haven't been created
+    create_database()
+    exit()
 
   print "Probing service has started!"
   print "Press CTRL+C to stop the server."
